d643ad478948793aafa0ec98374d64f043382767
Ephratah63 <ephratah.girma@aastustudent.edu.et>
2025-04-21 20:27:04 +0300

    Update sourcecode.cpp
    Test suspect module

--- sourcecode.cpp
+++ sourcecode.cpp
@@ -3,12 +3,13 @@
 #include <mysql_connection.h>
 #include <cppconn/prepared_statement.h>
 #include <cppconn/resultset.h>
-
+#include "suspect_module.h"
 using namespace std;
 
-int main(){
 
-
-
-  return 0;
+int main() 
+{
+    //to check that the suspect module works
+    suspectMenu();
+    return 0;
 }
