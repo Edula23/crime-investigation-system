1a5b1658f5b08abecfabd7f21eeed3d8c1d4af1a
Edom Getahun <edom.getahun@aastustudent.edu.et>
2025-04-22 19:07:55 +0300

    Update sourcecode.cpp

--- sourcecode.cpp
+++ sourcecode.cpp
@@ -6,10 +6,27 @@
 #include "suspect_module.h"
 using namespace std;
 
+// Structures to represent a case
+struct Case {
+    string case_ID;            // Case number
+    string description;        // Description of the case
+    string status;             // Current status of the case (e.g., open, closed)
+    string date_reported;      // Date when the case was reported
+    string assigned_officer;   // Officer assigned to the case
+    string department;         // Department handling the case
+    string progress;           // Progress of the case
+};
+
+// Function prototypes
+
 
 int main() 
 {
+    
     //to check that the suspect module works
     suspectMenu();
+   
     return 0;
 }
+
+
